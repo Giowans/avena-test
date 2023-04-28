d043309169b560e1cef98843a42b36978a950aed
Giowans <gio_tails@live.com>
2023-04-28 15:24:04 -0600

    Controller for fetching data implemented.

---
+++ src/components/Table.jsx
@@ -0,0 +1,78 @@
+import Image from "next/image";
+import { motion } from 'framer-motion';
+
+const Table = ({ data, headers, className, headerClassName, sortData, sorted, deleteUser}) => {
+  //State
+
+  //Effects
+
+  return (
+    <table className={`flex flex-col ${className}`}>
+      <tbody>
+        <tr className={`flex flex-row justify-between p-4 border border-0 rounded-tl-lg rounded-tr-lg bg-slate-700 ${headerClassName}`}>
+          {headers.map((head, index) => {
+            if(head.isSortable)
+            {
+              return (
+                <th className="flex flex-row items-center justify-center text-sm select-none md:text-lg">
+                  {head.title}
+                  <motion.div
+                    whileTap={{scale: 0.92}} 
+                    className="ml-1 cursor-pointer md:ml-3" onClick={() => sortData()}
+                    animate = {{rotate: sorted === head.key ? 180 : 0}}
+                  >
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-4 h-4 md:h-6 md:w-6">
+                      <path strokeLinecap="round" strokeLinejoin="round" d="M15 11.25l-3-3m0 0l-3 3m3-3v7.5M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
+                    </svg>
+                  </motion.div>
+                </th>
+              )
+            }
+            else
+            {
+              return <th className="flex flex-row items-center text-sm select-none md:text-lg">{head.title}</th>
+            }
+          })}
+          <th className="font-bold select-none">Actions</th>
+        </tr>
+        {data.map((user, index) => {
+            return (
+              <tr
+                key={`userData-${index}`}
+                className={`flex flex-row items-center justify-between py-1 px-3 ${
+                  index % 2 === 0 ? "bg-stone-200" : "bg-stone-100"
+                }`}
+              >
+                <td>
+                  <Image
+                    className="rounded-lg"
+                    alt={`user-${index}`}
+                    src={user.picture.thumbnail}
+                    width={40}
+                    height={40}
+                  />
+                </td>
+                <td>{user.name.first}</td>
+                <td>{user.name.last}</td>
+                <td>{user.location.country}</td>
+                <td>
+                  <div>
+                    <button
+                      onClick={() => deleteUser(index)}
+                      className="p-2 text-sm duration-200 bg-red-300 rounded-lg cursor-pointer hover:bg-red-200"
+                    >
+                      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-4 h-4 md:w-6 md:h-6">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
+                      </svg>
+                    </button>
+                  </div>
+                </td>
+              </tr>
+            );
+          })}
+      </tbody>
+    </table>
+  )
+}
+
+export default Table;
---
+++ src/pages/index.jsx
@@ -0,0 +1,209 @@
+import { useEffect, useState } from "react";
+import { motion } from "framer-motion";
+import Table from "../components/Table";
+
+function Home({ data }) {
+  const [dataState, setDataState] = useState([]);
+  const [sorted, setSorted] = useState('');
+  const [currentPage, setCurrentPage] = useState(1);
+  const [displayQuant, setDisplayQuant] = useState(10);
+  const [filterText, setFilterText] = useState("");
+
+  useEffect(() => {
+    setDataState(data);
+  }, []);
+
+  const deleteUser = (userIndex) => {
+    const refer = [...dataState];
+    refer.splice(userIndex, 1);
+    setDataState(refer);
+  };
+
+  const fetchData = async () => {
+
+  };
+
+  const handlePageChange = (option) => {
+    if (option === "minus") {
+      setCurrentPage((val) => val - 1);
+    }
+    if (option === "plus") {
+      setCurrentPage((val) => val + 1);
+    }
+  };
+
+  const sortData = (key) => {
+    if(key === sorted)
+    {
+      return;
+    }
+    const refer = [...dataState];
+    if(key === 'country')
+    {
+      refer.sort((a, b) => {
+        let ca = a.location.country.toLowerCase();
+        let cb = b.location.country.toLowerCase();
+        if (ca < cb) {
+          return -1;
+        }
+        if (ca > cb) {
+          return 1;
+        }
+        return 0;
+      });
+    }
+    if(key === '´first_name')
+    {
+      refer.sort((a, b) => {
+        let ca = a.name.first.toLowerCase();
+        let cb = b.name.first.toLowerCase();
+        if (ca < cb) {
+          return -1;
+        }
+        if (ca > cb) {
+          return 1;
+        }
+        return 0;
+      });
+    }
+    setDataState(refer);
+    setSorted(key);
+  };
+
+  return (
+    <main className="flex flex-col items-center justify-start min-h-screen px-8 py-12">
+      <h1 className="mt-4 text-2xl font-bold md:text-4xl">
+        Avena.io test challenge
+      </h1>
+      <button
+        onClick={() => setDataState(data)}
+        className="p-2 my-4 text-xl rounded-lg shadow-lg shadow-slate-600 bg-slate-300"
+      >
+        Reset Data
+      </button>
+      {/** Data Table Controllers */}
+      <div className="flex flex-col justify-between w-full md:flex-row md:w-[60rem] items-center">
+        <div className="flex flex-row items-center justify-start w-fit h-fit">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={2.5}
+            stroke="currentColor"
+            className={`w-6 h-6 ${document.activeElement.id === "search" ? 'text-black' : 'text-slate-400'}`}
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z"
+            />
+          </svg>
+          <input
+            id="search" 
+            placeholder="Buscar..."
+            onBlur={(e) => setFilterText(e.target.value)} 
+            className="px-2 py-1 ml-2 border-2 border-solid rounded-lg select-none border-slate-500 h-fit" 
+          />
+        </div>
+        <div className="flex flex-row items-center justify-start w-fit h-fit">
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className={`w-6 h-6 ${document.activeElement.id === "quant" ? 'text-black' : 'text-slate-400'}`}>
+            <path strokeLinecap="round" strokeLinejoin="round" d="M8.25 6.75h12M8.25 12h12m-12 5.25h12M3.75 6.75h.007v.008H3.75V6.75zm.375 0a.375.375 0 11-.75 0 .375.375 0 01.75 0zM3.75 12h.007v.008H3.75V12zm.375 0a.375.375 0 11-.75 0 .375.375 0 01.75 0zm-.375 5.25h.007v.008H3.75v-.008zm.375 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z" />
+          </svg>
+
+          <input 
+            placeholder="Cantidad..."
+            id="quant"
+            type="number"
+            onBlur={(e) => setFilterText(e.target.value)} 
+            className="px-2 py-1 ml-2 border-2 border-solid rounded-lg select-none border-slate-500 h-fit" 
+          />
+        </div>
+        <div className="flex flex-row items-center justify-between py-1 px-2 self-center w-[12rem] md:w-[8rem] my-3 md:self-end">
+          <motion.button
+            disabled={currentPage === 1}
+            whileTap={{ scale: 0.92 }}
+            className="cursor-pointer"
+            onClick={() => handlePageChange("minus")}
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="w-6 h-6 text-slate-500"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M11.25 9l-3 3m0 0l3 3m-3-3h7.5M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+              />
+            </svg>
+          </motion.button>
+
+          <h3 className="text-lg font-bold select-none">{currentPage}</h3>
+
+          <motion.button
+            whileTap={{ scale: 0.92 }}
+            className="cursor-pointer"
+            onClick={() => handlePageChange("plus")}
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="w-6 h-6 text-slate-500"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M12.75 15l3-3m0 0l-3-3m3 3h-7.5M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+              />
+            </svg>
+          </motion.button>
+        </div>
+      </div>
+      <Table
+        data={dataState}
+        sorted={sorted}
+        headers={[
+          {
+            title: "Photo",
+            key: "photo",
+            isSortable: false,
+          },
+          {
+            title: "First Name",
+            key: "first_name",
+            isSortable: false,
+          },
+          {
+            title: "Last Name",
+            key: "last_name",
+            isSortable: false,
+          },
+          {
+            title: "Country",
+            key: "country",
+            isSortable: true,
+          },
+        ]}
+        sortData={sortData}
+        deleteUser={deleteUser}
+        className={`w-full md:h-[33rem] md:w-[60rem] mt-2`}
+        headerClassName={"bg-slate-300"}
+      />
+    </main>
+  );
+}
+
+export async function getServerSideProps() {
+  const res = await fetch("https://randomuser.me/api/?results=100");
+  const data = await res.json();
+
+  return { props: { data: data.results } };
+}
+
+export default Home;
